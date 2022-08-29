0ceed5def80857158bdf9b5236da4ca9f59dc037
Lyubov Shamanina <82117621+sha-ma-ni@users.noreply.github.com>
2022-08-29 15:24:52 +0200

    server listening

--- test/test.js
+++ test/test.js
@@ -1,4 +1,19 @@
 const assert = require('assert')
-it('should return true', () => {
-    assert.equal(true, true)
-})
+const waitPort = require('wait-port');
+const PORT = process.env.PORT || 8080;
+const {expect} = require('chai');
+
+describe('server listening', () => {
+    it('should be listening', async () => {
+        const server = require ('../server.js');
+        const isOpen = await waitPort({port: PORT});
+        expect(isOpen).to.be.true;
+        await timeOut();
+        function timeOut(ms) {
+            return new Promise(
+                resolve => setTimeout(resolve, 1000)
+            )
+        }
+    });
+
+});
