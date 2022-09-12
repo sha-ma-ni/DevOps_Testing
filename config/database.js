c7fdbbab4e5458f34beaab327adfdca6f87c5ba4
Katharina <katharinaschippl@htw-berlin.de>
2022-09-12 17:27:43 +0200

    user account for database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb+srv://anny:annyschlaeft@todo.wkqpuai.mongodb.net/?retryWrites=true&w=majority',
+    remoteUrl : 'mongodb+srv://UserTODO:UserTODO@cluster0.z6kleok.mongodb.net/test',
     //localUrl: 'mongodb://localhost:27017',
 };
