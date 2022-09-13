c1be9172ec3f4d4eee6ab3fefe8e9acb5b777c51
Lyubov Shamanina <82117621+sha-ma-ni@users.noreply.github.com>
2022-09-13 22:59:45 +0200

    database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb+srv://UserTODO:UserTODO@cluster0.z6kleok.mongodb.net/?retryWrites=true&w=majority',
+    remoteUrl : 'mongodb+srv://UserTODO:UserTODO@cluster0.z6kleok.mongodb.net/?retryWrites=true&w=majority/test',
     //remoteUrl : 'mongodb+srv://UserTODO:UserTODO@cluster0.z6kleok.mongodb.net/test';
     //localUrl: 'mongodb://localhost:27017',
 };
