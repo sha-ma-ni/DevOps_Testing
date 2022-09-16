be55b859c2997379c71af9fbb24a3fc9daef9e00
Katharina <katharinaschippl@htw-berlin.de>
2022-09-16 15:30:12 +0200

    database config new

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb+srv://anny:annyschlaeft@todo.wkqpuai.mongodb.net/?retryWrites=true&w=majority',
+    remoteUrl : 'mongodb+srv://anny:annyschlaeft@todos.1mvc8io.mongodb.net/?retryWrites=true&w=majority',
     //remoteUrl : 'mongodb+srv://UserTODO:UserTODO@cluster0.z6kleok.mongodb.net/test';
     //localUrl: 'mongodb://localhost:27017',
 };
