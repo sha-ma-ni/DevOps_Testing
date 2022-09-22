57d36e7989e5acbf1be50ffd673b4f33a95d98c5
Katharina <katharinaschippl@htw-berlin.de>
2022-09-23 01:23:17 +0200

    small change in error test

--- app/routes.js
+++ app/routes.js
@@ -55,7 +55,7 @@ module.exports = function (app) {
     //     res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     // });
     //ERROR-MESSAGE
-    app.delete('/api/todos/:todo_id', function(req, res) {
+    app.get('/api/todos/:todo_id', function(req, res) {
         const todo = req.params.todo;
         if(!todo == todo_id) {
             console.log('todo_id not found');
--- test/Error.test.js
+++ test/Error.test.js
@@ -6,7 +6,7 @@ describe('ERROR 404 testing', () => {
     it('should return 404 if todo not found', (done) => {
         let iwas = new ObjectId()
         request(app)
-        .delete('/api/todos/:todo_id/${api/todos/iwas}')
+        .get('/api/todos/:todo_id/${api/todos/iwas}')
         .expect(404)
         .end(done)
     })
