7c542b685e4bc90c356f36a9d5781b7672fcf440
Katharina <katharinaschippl@htw-berlin.de>
2022-09-23 02:20:41 +0200

    small change in error test

--- test/Error.test.js
+++ test/Error.test.js
@@ -4,7 +4,7 @@ const app = require('./../server')
 
 describe('ERROR 404 testing', () => {
     it('should return 404 if todo not found', (done) => {
-        let iwas = new ObjectId()
+        let iwas = new ObjectId("6329a0cf42438c7405488888")
         request(app)
         .get('/api/todos/:todo_id/${api/todos/iwas}')
         .expect(404)
